d0d33d9e3f7e9230089ef1b817e6a8982e28823f
Daniel Bergey <bergey@simspace.com>
2020-01-24 08:49:48 -0500

    stack language with Int & Double, compile to it

---
+++ src/Compilers/DoubleStack.hs
@@ -0,0 +1,47 @@
+-- | Compile the two-type expression language to a stack language.
+-- Type errors should be caught during compilation, not during
+-- execution of the stack program.
+
+module Compilers.DoubleStack where
+
+import qualified Interpreters.BinDouble as E
+import qualified Interpreters.StackDouble as T
+
+-- | This does no type checking, and does not ever raise errors
+unsafeCompile :: E.Expr -> [T.Instruction]
+unsafeCompile (E.I i) = [T.Push (T.I i)]
+unsafeCompile (E.D x) = [T.Push (T.D x)]
+unsafeCompile (E.Op op a b) = op' : unsafeCompile a ++ unsafeCompile b
+    where op' = case op of
+            E.Add -> T.Op T.Add
+            E.Sub -> T.Op T.Sub
+            E.Mul -> T.Op T.Mul
+            E.Div -> T.Op T.Div
+
+-- | Interleaves type checking with compilation.  Returns the
+-- instructions of a stack program, and the type left at the top of
+-- the stack after running the program.
+safeCompile :: E.Expr -> Either (E.Error E.PrimType) (E.PrimType, [T.Instruction])
+safeCompile (E.I i) = Right (E.TyInt, [T.Push (T.I i)])
+safeCompile (E.D x) = Right (E.TyDouble, [T.Push (T.D x)])
+safeCompile (E.Op op a b) = do
+    a' <- safeCompile a
+    b' <- safeCompile b
+    resultType <- case (op, fst a', fst b') of
+        (E.Div, E.TyDouble, E.TyDouble) -> Right E.TyDouble
+        (E.Div, aTy, bTy) -> Left (E.IntDivision aTy bTy)
+        (_, aTy, bTy) -> if aTy == bTy then Right aTy else Left (E.TypeMismatch aTy bTy)
+    let op' = case op of
+            E.Add -> T.Add
+            E.Sub -> T.Sub
+            E.Mul -> T.Mul
+            E.Div -> T.Div
+    return (resultType, T.Op op' : snd a' ++ snd b')
+
+-- | Also safe, but type checks before compiling instead of
+-- interleaving We don't need the PrimType in the result; it's just
+-- nice for this to have the same type as @safeCompile@
+safeCompile2 :: E.Expr -> Either (E.Error E.PrimType) (E.PrimType, [T.Instruction])
+safeCompile2 expr = do
+    resultType <- E.typeCheck expr
+    return (resultType, unsafeCompile expr)
---
+++ src/Interpreters/StackDouble.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE RankNTypes #-}
+-- | A stack language, just barely fancier than the one in Interpreters.Stack.
+
+module Interpreters.StackDouble where
+
+import Prelude
+
+data PrimValue = I Int | D Double
+    deriving (Show, Eq)
+
+data Instruction = Push PrimValue | Pop | Op BinOp
+    deriving (Show, Eq)
+
+data BinOp = Add | Sub | Mul | Div
+    deriving (Show, Eq)
+
+data Error = Underflow | TypeMismatch PrimValue PrimValue | IntDivision PrimValue PrimValue
+
+type Stack = [PrimValue]
+
+eval :: Stack -> [Instruction] -> Either Error Stack
+eval xs (Push x : is) = eval (x : xs) is
+eval (_ : xs) (Pop : is) = eval xs is
+eval [] (Pop : _) = Left Underflow
+eval (x : y : xs) (Op op : is) = do
+    r <- f x y
+    eval (r : xs) is
+  where
+    f x y = case op of
+        Add -> lift (+) x y
+        Sub -> lift (-) x y
+        Mul -> lift (*) x y
+        Div -> case (x, y) of
+            (D x, D y) -> Right (D (x / y))
+            _ -> Left (IntDivision x y)
+    lift :: (forall a. Num a => a -> a -> a) -> PrimValue -> PrimValue -> Either Error PrimValue
+    lift op x y = case (x, y) of
+        (I i, I j) -> Right (I (op i j))
+        (D x, D y) -> Right (D (op x y))
+        _ -> Left (TypeMismatch x y)
+
+eval _ (Op _ : _) = Left Underflow
+eval xs [] = return xs
+
+run :: [Instruction] -> Either Error Stack
+run = eval []
